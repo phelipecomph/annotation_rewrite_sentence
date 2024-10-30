97bb8e99af2ae7801629d4df1760e01cd840f206
Phelipe Müller <phelipecomph42@gmail.com>
2024-10-30 17:07:10 -0300

    fix: credentials os.env to st.secrets

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import boto3
 import os
 
 s3 = boto3.client('s3',
-                  aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-                  aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+                  aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+                  aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"])
 
 bucket_name = 'static.redacaonota1000.com.br'
 file_path = 'pares_de_sentenca.csv'
